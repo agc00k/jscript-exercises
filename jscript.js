1ba2af442accd4ec34bc863744de53141997a0f4
agc00k <246743604+agc00k@users.noreply.github.com>
2025-12-07 19:18:36 -0800

    my javascript exercise files v.1

--- jscript.js
+++ jscript.js
@@ -1,7 +1,4 @@
 // JavaScript File
-let hour = 12;
-let minute = 30;
-
-if (hour == 12 && minute == 30) {
-  alert( 'The time is 12:30' );
+function handleAnimalChage(selectedValue) {
+  alert('My Spirit Animal is: ' + selectedValue);
 }
